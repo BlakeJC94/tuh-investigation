4a3734ff0651f5ac74484563d0a58e7fbe41ddf2
Blake <blakejamescook@gmail.com>
2020-06-23 20:45:40 +1000

    cleaned up code

--- src/MATLAB/eegdiffplot.m
+++ src/MATLAB/eegdiffplot.m
@@ -1,7 +1,7 @@
-function eegdiffplot(filename, timespan)
+function eegdiffplot(fileName, timeSpan)
 % Plots Fz and Cz in two different colours across two timespans
-% filename : str
-% timespan : 1x3 double (seconds)
+% fileName : str
+% timeSpan : 1x3 double (seconds)
 
 
 %% Load dependencies and set globals %%%%%%%%
@@ -13,30 +13,33 @@ if ~exist(outputDir, 'dir')
     mkdir(outputDir)
 end
 
+
+
+
 %% Load file %%%%%%%%
 
 % find directory
 allfiles = struct2cell(dir('./edf/dev/**/*.edf'));
 for ind = 1:size(allfiles,2)
     label = allfiles{1,ind};
-    if ~isempty(regexp(label, filename, "once"))
-        filedir = allfiles{2,ind};
+    if ~isempty(regexp(label, fileName, "once"))
+        fileDir = allfiles{2,ind};
         break
     end
 end
 
-% error out if the filename isn't found
-if ~exist('filedir')
+% error out if fileName isn't found
+if ~exist('fileDir')
     error('B: file not found')
 end
 
 % load edf
-[hdr, record] = edfread(strcat(filedir, '/', filename));
+[hdr, record] = edfread(strcat(fileDir, '/', fileName));
 
 % extract channels of interest
 FZch = findChannel(hdr, "[Ff][Zz]");
-FZdata = record(FZch, :);
 CZch = findChannel(hdr, "[Cc][Zz]");
+FZdata = record(FZch, :);
 CZdata = record(CZch, :);
 
 
@@ -46,51 +49,65 @@ CZdata = record(CZch, :);
 
 FZfreq = hdr.frequency(FZch);
 CZfreq = hdr.frequency(FZch);
-
 if FZfreq == CZfreq
     freq = FZfreq;
 else
-    error("Frequencies are differnt??")
+    error("Frequencies are different??")
 end
 
-sample1 = (max(fix(freq*timespan(1)),1)):(fix(freq*timespan(2)));
-sample2 = (max(fix(freq*timespan(2)),1)):(fix(freq*timespan(3)));
+% get sample points of requested timeSpan
+sample1 = (...
+    max(fix(freq*timeSpan(1)),1)):...
+    fix(freq*timeSpan(2)...
+);
+sample2 = (...
+    max(fix(freq*timeSpan(2)),1)):...
+    fix(freq*timeSpan(3)...
+);
+
+
 
 
 %% Calculate distance, speed, and acceleration %%%%%%%%
 
-timeseries = [FZdata(:), CZdata(:)];
+timeSeries = [FZdata(:), CZdata(:)];  % indexed by (sample, position)
 
-distance = vecnorm(cumsum(timeseries(:, [1,2]), 1)/freq, 2, 2);
-speed = vecnorm(timeseries(:, [1,2]), 2, 2);
-accel = vecnorm(diff(timeseries(:, [1,2]), 1)*freq, 2, 2);
-
+distance = vecnorm(...
+    cumsum(timeSeries(:, [1,2]), 1)/freq,...
+    2, 2
+);
+speed = vecnorm(...
+    timeSeries(:, [1,2]),...
+    2, 2...
+);
+accel = vecnorm(...
+    diff(timeSeries(:, [1,2]), 1)*freq,...
+    2, 2...
+);
 
 % save data to mat files
-dataName = strcat(filename(1:end-4), '_', num2str(timespan(1)), '.mat');
-
+dataName = strcat(fileName(1:end-4), '_', num2str(timeSpan(1)), '.mat');
 dataDir = strcat(outputDir, 'data/');
 if ~exist(dataDir, 'dir')
     mkdir(dataDir)
 end
 save(...
     strcat(dataDir, dataName),...
-    'filename', 'FZdata', 'CZdata',...
+    'fileName', 'FZdata', 'CZdata',...
     'freq', 'sample1', 'sample2',...
     'distance', 'speed', 'accel'...
 );
 
 
 
+
 %% Plot data %%%%%%%%
 
-h = figure(1);
-clf
-
+h = figure(1); clf;
 plotx      = 50;   % Screen position
 ploty      = 50;   % Screen position
 plotwidth  = 1800; % Width of figure
-plotheight = 700; % Height of figure (by default in pixels)
+plotheight = 700;  % Height of figure (by default in pixels)
 set(h, 'Position', [plotx ploty plotwidth plotheight]);
 
 % phase plot
@@ -106,13 +123,13 @@ plotmax = max(max(FZdata([sample1, sample2])), max(CZdata([sample1, sample2])));
 axis([plotmin, plotmax, plotmin, plotmax]);
 grid on;
 % write title and axis labels
-titlestr = strcat(filename, ' freq = ', num2str(freq));
+titlestr = strcat(fileName, ' freq = ', num2str(freq));
 title(titlestr, 'Interpreter', 'none');
 xlabel("Fz-ref (\muV)");
 ylabel("Cz-ref (\muV)");
 % write legend
-legendstr1 = strcat(num2str(timespan(1)), 's -- ', num2str(timespan(2)), 's');
-legendstr2 = strcat(num2str(timespan(2)), 's -- ', num2str(timespan(3)), 's');
+legendstr1 = strcat(num2str(timeSpan(1)), 's -- ', num2str(timeSpan(2)), 's');
+legendstr2 = strcat(num2str(timeSpan(2)), 's -- ', num2str(timeSpan(3)), 's');
 legend(legendstr1, legendstr2)
 
 
@@ -141,7 +158,7 @@ ylabel("Cz-ref (\muV)");
 
 
 % save to disk
-plotName = strcat(filename(1:end-4), '_', num2str(timespan(1)), '.png');
+plotName = strcat(fileName(1:end-4), '_', num2str(timeSpan(1)), '.png');
 plotDir = strcat(outputDir, 'phase/');
 if ~exist(plotDir, 'dir')
     mkdir(plotDir)
@@ -149,38 +166,50 @@ end
 saveas(gcf,strcat(plotDir, plotName));
 
 
-%% Plot acceleration %%%%%%%%
 
-figure(2); clf;
+
+%% Plot motion %%%%%%%%
+
+h = figure(2); clf;
 plotx      = 50;   % Screen position
 ploty      = 50;   % Screen position
-plotwidth  = 900; % Width of figure
+plotwidth  = 900;  % Width of figure
 plotheight = 1200; % Height of figure (by default in pixels)
 set(gcf, 'Position', [plotx ploty plotwidth plotheight]);
 
+
+% Plot distance
 subplot(3, 1, 1);
 hold on
 plot(seconds(sample1/CZfreq), distance(sample1), 'b');
 plot(seconds(sample2/CZfreq), distance(sample2), 'r');
 hold off
+% write title and axis labels
 title("``dist''"); xlabel("Time"); ylabel("\muV s");
 
+
+% Plot speed
 subplot(3, 1, 2);
 hold on
 plot(seconds(sample1/CZfreq), speed(sample1), 'b');
 plot(seconds(sample2/CZfreq), speed(sample2), 'r');
 hold off
+% write title and axis labels
 title("``speed''"); xlabel("Time"); ylabel("\muV");
 
+
+% Plot acceleration
 subplot(3, 1, 3);
 hold on
 plot(seconds(sample1/CZfreq), accel(sample1), 'b');
 plot(seconds(sample2(1:end-1)/CZfreq), accel(sample2(1:end-1)), 'r');
 hold off
+% write title and axis labels
 title("``accel''"); xlabel("Time"); ylabel("\muV s^{-1}");
 
+
 % save to disk
-plotName = strcat(filename(1:end-4), '_', num2str(timespan(1)), '.png');
+plotName = strcat(fileName(1:end-4), '_', num2str(timeSpan(1)), '.png');
 plotDir = strcat(outputDir, 'accel/');
 if ~exist(plotDir, 'dir')
     mkdir(plotDir)
